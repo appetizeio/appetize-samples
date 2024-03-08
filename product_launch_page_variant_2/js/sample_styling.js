955d81bcdc3b6eee27f5ff52701f8369c7814537
Pieter Snyman <dageforce@users.noreply.github.com>
2024-03-08 15:40:15 +0200

    Add a way to pass publicKeys to our product launch samples (#7)

--- product_launch_page_variant_2/js/sample_styling.js
+++ product_launch_page_variant_2/js/sample_styling.js
@@ -14,6 +14,41 @@ const updateLogoFromQueryParam = () => {
 };
 
 /**
+ * Updates the config products to match the apps passed in the query parameters.
+ */
+function updateConfigProducts() {
+    const optionalAndroidPublicKey = queryParams.get('optionalAndroidPublicKey');
+    const optionaliOSPublicKey = queryParams.get('optionaliOSPublicKey');
+    const optionalAppName = queryParams.get('optionalAppName');
+
+    if (!optionalAndroidPublicKey && !optionaliOSPublicKey) {
+        return;
+    }
+
+    config.products = [];
+
+    if (optionaliOSPublicKey) {
+        const name = optionalAppName + " (iOS)" ?? "iOS Demo App";
+        const product = {
+            name: name,
+            publicKey: optionaliOSPublicKey,
+            device: "iphone15pro",
+        };
+        config.products.push(product);
+    }
+
+    if (optionalAndroidPublicKey) {
+        const name = optionalAppName + " (Android)" ?? "Android Demo App";
+        const product = {
+            name: name,
+            publicKey: optionalAndroidPublicKey,
+            device: "pixel6",
+        };
+        config.products.push(product);
+    }
+}
+
+/**
  * Updates a CSS variable from a query parameter.
  * @param variableName The name of the CSS variable.
  * @param queryParamName The name of the query parameter.
@@ -30,3 +65,4 @@ document.addEventListener('DOMContentLoaded', updateLogoFromQueryParam);
 updateCSSVariable('--bs-primary', 'primaryColor');
 updateCSSVariable('--bs-primary-dark', 'primaryColorDark');
 updateCSSVariable('--bs-foreground', 'primaryForegroundColor');
+updateConfigProducts();
