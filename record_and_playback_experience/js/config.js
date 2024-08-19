ae465c2a72c8c432f953e27a8571e5dcdde72769
Augusto <augustoalonso98@gmail.com>
2024-08-19 15:08:47 -0600

    - Move samples cards to be generated from samples.js - Save video recording experience - Record actions experience

---
+++ record_and_playback_experience/js/config.js
@@ -0,0 +1,14 @@
+// Description: This file contains the configuration for the record and playback experience.
+
+const config = {
+    products: [
+        {
+            platform: 'iOS',
+            publicKey: 'demo_phq04c56jnvrkg0bn9w5ep4m9r'
+        },
+        {
+            platform: 'Android',
+            publicKey: 'demo_r0m5r98axtdhftx1hmmhq1c0m8'
+        },
+    ]
+}
---
+++ save_video_recording_experience/js/config.js
@@ -0,0 +1,5 @@
+// Description: This file contains the configuration for the save video recording launch template.
+
+const config = {
+    publicKey: "demo_phq04c56jnvrkg0bn9w5ep4m9r"
+}
