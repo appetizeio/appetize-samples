ae1bed18d8f032e69ce6ad2ece8011a592841447
Augusto <augustoalonso98@gmail.com>
2024-09-23 20:03:50 -0600

    Fix optional key behavior for the playback experience

--- record_and_playback_experience/js/config.js
+++ record_and_playback_experience/js/config.js
@@ -4,11 +4,13 @@ const config = {
     products: [
         {
             platform: 'iOS',
-            buildId: 'demo_phq04c56jnvrkg0bn9w5ep4m9r'
+            buildId: 'demo_phq04c56jnvrkg0bn9w5ep4m9r',
+            device: "iphone15pro"
         },
         {
             platform: 'Android',
-            buildId: 'demo_r0m5r98axtdhftx1hmmhq1c0m8'
+            buildId: 'demo_r0m5r98axtdhftx1hmmhq1c0m8',
+            device: "pixel7"
         },
     ]
 }
