9d8a68352cdeeecb46b15c3f6679edede94f51e7
Augusto <augustoalonso98@gmail.com>
2024-09-23 20:12:17 -0600

    Run prettifier on record & playback experience

--- record_and_playback_experience/js/config.js
+++ record_and_playback_experience/js/config.js
@@ -1,16 +1,16 @@
 // Description: This file contains the configuration for the record and playback experience.
 
 const config = {
-    products: [
-        {
-            platform: 'iOS',
-            buildId: 'demo_phq04c56jnvrkg0bn9w5ep4m9r',
-            device: "iphone15pro"
-        },
-        {
-            platform: 'Android',
-            buildId: 'demo_r0m5r98axtdhftx1hmmhq1c0m8',
-            device: "pixel7"
-        },
-    ]
-}
+  products: [
+    {
+      platform: "iOS",
+      buildId: "demo_phq04c56jnvrkg0bn9w5ep4m9r",
+      device: "iphone15pro",
+    },
+    {
+      platform: "Android",
+      buildId: "demo_r0m5r98axtdhftx1hmmhq1c0m8",
+      device: "pixel7",
+    },
+  ],
+};
